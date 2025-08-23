1e4d8f77cf9675632d01aba808b2bc4df789768c
chaehyung <surveyk79@gmail.com>
2025-08-23 17:01:36 +0900

    커밋 메시지 작성

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-from langchain.chat_models import init_chat_model
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-load_dotenv()
-
-#ChatOpenAI 초기화
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
-
-#프롬프트 템플릿
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant."),
-    ("user", "{input}")
-])
-
-#문자열 출력 파서
-output_parser = StrOutputParser()
-
-chain = prompt | llm | output_parser
-result = chain.invoke({"input": "hi"})
-print(result)
-=======
-# test.py
 import streamlit as st
 #from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
@@ -72,4 +48,3 @@ if st.button("n행시 생성"):
 
         except Exception as e:
             st.error(f"오류 발생: {e}")
->>>>>>> ce96c00 (추가)
